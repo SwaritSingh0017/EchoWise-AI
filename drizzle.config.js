a6e37b3e22ce186a2be366b40bb6c903fdad9f95
Ayushgourav17 <ayush.gourav09@gmail.com>
2026-03-21 17:18:01 +0530

    ayush

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,9 +3,9 @@ export default {
     schema: "./src/utils/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-      url: "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require",
+      url: "postgresql://neondb_owner:npg_C46rWitKUNAp@ep-rough-tree-anddtbqo-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
       connectionString:
-        "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require",
+        "postgresql://neondb_owner:npg_C46rWitKUNAp@ep-rough-tree-anddtbqo-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
     },
   };
   
--- src/utils/db/dbConfig.jsx
+++ src/utils/db/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://zerotohero_owner:SPVe2rokvBf6@ep-bitter-shadow-a5lbwa60.us-east-2.aws.neon.tech/zerotohero?sslmode=require"
+  "postgresql://neondb_owner:npg_C46rWitKUNAp@ep-rough-tree-anddtbqo-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 );
 export const db = drizzle(sql, { schema });
